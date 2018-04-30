df4003cfe0e1a129905b3bf9838aa88e902d3e1b
Cassandra Torske <cassandra.torske@gmail.com>
2018-04-29 21:59:05 -0600

    fuzzy

--- main.js
+++ main.js
@@ -5,21 +5,21 @@ const mouseDiv = document.getElementById('mousey')
   let height = window.innerHeight
 
   function calculateLightX(x) {
-    let percX = x / width
+    let percX = (x - 15) / width
     percX = percX * 100
     return `${percX}%`
   }
 
   function calculateLightY(y) {
-    let percY = y / height
+    let percY = (y + 15) / height
     percY = percY * 100
     return `${percY}%`
   }
 
-all.addEventListener('mouseover', (e) => {
+all.addEventListener('mousemove', (e) => {
   var x = e.clientX
   var y = e.clientY
   let calculatedX = calculateLightX(x)
   let calculatedY = calculateLightY(y)
-  mouseDiv.style.backgroundImage = `-webkit-radial-gradient(${calculatedX} ${calculatedY}, circle, transparent 50px, black 0)`
+  mouseDiv.style.backgroundImage = `-webkit-radial-gradient(${calculatedX} ${calculatedY}, circle, transparent 50px, black)`
 })
